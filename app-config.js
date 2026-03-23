3fe8e45b0e67802d63ffea112895fbcb72dd6137
belzarena-like <belzarena@outlook.es>
2026-03-23 09:12:42 +0100

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,11 @@
+(function () {
+  var existing = window.APP_CONFIG || {};
+  var override = window.LIKESTUDIO_API_BASE_URL;
+  var defaultBase = "https://apis.listoapp.es/like_api";
+
+  var baseUrl = (override || existing.API_BASE_URL || defaultBase).replace(/\/+$/, "");
+
+  window.APP_CONFIG = {
+    API_BASE_URL: baseUrl
+  };
+})();
