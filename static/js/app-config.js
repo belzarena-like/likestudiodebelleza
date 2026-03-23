59631b289b8ed2131074d2e5e47146dc94f73d86
belzarena-like <belzarena@outlook.es>
2026-03-23 09:22:24 +0100

    Update app-config.js

--- static/js/app-config.js
+++ static/js/app-config.js
@@ -1,11 +1,16 @@
 (function () {
   var existing = window.APP_CONFIG || {};
   var override = window.LIKESTUDIO_API_BASE_URL;
-  var defaultBase = "http://localhost:8000";
+  var defaultBase = "https://apis.listoapp.es/like_api";
+
 
   var baseUrl = (override || existing.API_BASE_URL || defaultBase).replace(/\/+$/, "");
 
+  
   window.APP_CONFIG = {
+  
     API_BASE_URL: baseUrl
   };
+
+
 })();
